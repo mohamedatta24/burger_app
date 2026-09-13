682130924d02a9ebdb6e863f12e0a64eb93d33aa
Mohamed Atta <mohamedatta24@gmail.com>
2026-09-13 15:41:19 +0300

    finsh splash

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color black = Color(0xff131214);
+}
---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:burger_app/core/utils/app_images.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [SvgPicture.asset(Assets.imagesLogoBurger)],
+      ),
+    );
+  }
+}
